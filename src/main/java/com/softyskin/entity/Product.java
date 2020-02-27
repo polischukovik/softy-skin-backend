09303288aa4f85aefa3caaf9e0688df1cbf262fd
polischukovik <polischuk@gmail.com>
2020-02-27 02:09:10 +0200

    added findByUuid

--- src/main/java/com/softyskin/entity/Product.java
+++ src/main/java/com/softyskin/entity/Product.java
@@ -1,36 +1,30 @@
 package com.softyskin.entity;
 
 import org.bson.types.Binary;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "products")
 public class Product {
 	@Id
-	private String _id;
-	
-	private String uuid;
-	
-	private String name;
-	
-	private String description;
-	
-	private String size;
-	
-	private Double price;
-	
-	private Integer position;
-	
+	private ObjectId id;
+	private String uuid;	
+	private String name;	
+	private String description;	
+	private String size;	
+	private Double price;	
+	private Integer position;	
 	private Binary image;
-
-	public String get_id() {
-		return _id;
-	}
-
-	public void set_id(String _id) {
-		this._id = _id;
-	}
 	
+	public ObjectId getId() {
+		return id;
+	}
+
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
 	public String getUuid() {
 		return uuid;
 	}
--- src/main/java/com/softyskin/repository/ProductRepository.java
+++ src/main/java/com/softyskin/repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.softyskin.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.softyskin.entity.Product;
 
 @Repository
 public interface ProductRepository extends MongoRepository<Product, String>{
+	Product findByUuid(@Param("uuid") String uuid);
 }
